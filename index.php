829811e845255c779a4454faa1f762af1de15cff
Emilie Chartier <emilie.chartier@sfr.fr>
2016-11-21 14:03:30 +0100

    ajout photos et css

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 <link href='http://fonts.googleapis.com/css?family=Montserrat|Cardo' rel='stylesheet' type='text/css'>
 <!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
 <link rel="stylesheet" href="css/style.css">
 
   
@@ -29,9 +29,7 @@
         <a class="logo" href="index.php"><img src="img/logo.png"></a>
 
         <div class="mobile-toggle">
-            <span></span>
-            <span></span>
-            <span></span>
+            
         </div>
 
         <nav>
@@ -49,8 +47,10 @@
 
 <div class="hero">
 
-    <h1><span>Thymerais'nov</span><br>Maçonnerie Générale</h1>
-
+    <h1><span>Thymerais'nov</span><br>Maçonnerie Générale<br>
+        06 11 31 47 41
+    </h1>
+    
     
 
 </div>
@@ -100,8 +100,6 @@
         </div>
     </div>
   </div>
-
-  <!-- Controls -->
   
 </div>
 </div>
@@ -113,191 +111,232 @@
     A la tête de la société : Monsieur Leroi et Monsieur Fortier, maçons passionnés par leur métier.</p>
   <p><a class="btn btn-info" href="#" role="button">En savoir plus</a></p>
 </div>
+
     <h1 class="sec03">Photos</h1>
     
-    <div class="page-header">
-        <h1>Réalisations</h1>
-          
+    <section class="image-wrap cf clear" id="image-wrap">
+<a href="#test1Content" class="group-cbox1 inline" title="Pavés"><img alt="paves" src="img/paves.jpg" class="shadow" ></a>
+<a href="#test2Content" class="group-cbox1 inline" title="Terrasse"><img alt="terrasse" src="img/terrasse.jpg" class="shadow"></a>
+<a href="#test3Content" class="group-cbox1 inline" title="Dallage"><img alt="dallage" src="img/dallage.jpg" class="shadow"></a>
+<a href="#test4Content" class="group-cbox1 inline" title="Brique"><img alt="brique" src="img/brique.jpg" class="shadow"></a>
+<a href="#test5Content" class="group-cbox1 inline" title="Cintre en brique"><img alt=" cintre en brique" src="img/brique1.jpg" class="shadow"></a>
+<a href="#test6Content" class="group-cbox1 inline" title="Appui en brique"><img alt="appui en brique" src="img/brique2.jpg" class="shadow"></a>
+<a href="#test7Content" class="group-cbox1 inline" title="Dalle"><img alt="dalle" src="img/dalle.jpg" class="shadow"></a>
+<a href="#test8Content" class="group-cbox1 inline" title="Façade"><img alt="facade" src="img/facade.jpg" class="shadow"></a>
+<a href="#test9Content" class="group-cbox1 inline" title="Dalle"><img alt="dalle" src="img/dallethimert.jpg" class="shadow"></a>
+<a href="#test10Content" class="group-cbox1 inline" title="Terrasse"><img alt="terrasse" src="img/terrasse1.jpg" class="shadow"></a>
+<a href="#test11Content" class="group-cbox1 inline" title="Tranchée"><img alt="tranchee" src="img/tranchee.jpg" class="shadow"></a>
+<a href="#test12Content" class="group-cbox1 inline" title="Terrasse"><img alt="terrasse" src="img/terrasse3.jpg" class="shadow"></a>
+<!--<a href="#test13Content" class="group-cbox1 inline" title="Lorem ipsum 13"><img alt="" src="http://placehold.it/128x128/" class="shadow"></a>
+<a href="#test14Content" class="group-cbox1 inline" title="Lorem ipsum 14"><img alt="" src="http://placehold.it/128x128/" class="shadow"></a>
+<a href="#test15Content" class="group-cbox1 inline" title="Lorem ipsum 15"><img alt="" src="http://placehold.it/128x128/" class="shadow"></a>-->
+
+<div style='display:none'>
+  <div id="test1Content" class="inner-overlay-box">
+    <div class="media">
+      <a href="#" class="img">
+        <img alt="paves" src="img/paves.jpg" class="img-inner-window" /></a>
+
+      <div class="hd"><h3>Pavés</h3></div>
+      <div class="bd">
+        <p class="p-inner-window">Mise en place de pavés en grès ocre beige.</p> </div>
     </div>
-    <!--
-    <div class="row active-with-click">
-        <div class="col-md-4 col-sm-6 col-xs-12">
-            <article class="material-card Red">
-                <h2>
-                    <span>Paves</span>
-                    
-                </h2>
-                <div class="mc-content">
-                    <div class="img-container">
-                        <img class="img-responsive" src="img/paves.jpg">
-                    </div>
-                    <div class="mc-description">
-                        Description
-                    </div>
-                </div>
-                <a class="mc-btn-action">
-                    <i class="fa fa-bars"></i>
-                </a>
-                <div class="mc-footer">
-                    <h4>
-                       
-                    </h4>
-                     Matériaux utilises : ...
-                </div>
-            </article>
-        </div>
-        <div class="col-md-4 col-sm-6 col-xs-12">
-            <article class="material-card Pink">
-                <h2>
-                    <span>Terrassement</span>
-                    
-                </h2>
-                <div class="mc-content">
-                    <div class="img-container">
-                        <img class="img-responsive" src="img/paves.jpg">
-                    </div>
-                    <div class="mc-description">
-                        Description
-                    </div>
-                </div>
-                <a class="mc-btn-action">
-                    <i class="fa fa-bars"></i>
-                </a>
-                <div class="mc-footer">
-                    <h4>
-                       
-                    </h4>
-                     Matériaux utilises : ...
-                </div>
-                </div>
-            </article>
-        </div>-->
-        <div class="col-md-4 col-sm-6 col-xs-12">
-            <article class="material-card Orange">
-                <h2>
-                    <span>Terrasse</span>
-                    
-                </h2>
-                <div class="mc-content">
-                    <div class="img-container">
-                        <img class="img-responsive" src="img/paves.jpg">
-                    </div>
-                    <div class="mc-description">
-                        Description
-                    </div>
-                </div>
-                <a class="mc-btn-action">
-                    <i class="fa fa-bars"></i>
-                </a>
-                
-            </article>
-        </div>
+  </div>
+</div>
 
-        <div class="col-md-4 col-sm-6 col-xs-12">
-            <article class="material-card Blue">
-                <h2>
-                    <span>Paves</span>
-                    
-                </h2>
-                <div class="mc-content">
-                    <div class="img-container">
-                        <img class="img-responsive" src="img/paves.jpg">
-                    </div>
-                    <div class="mc-description">
-                        Description
-                    </div>
-                </div>
-                <a class="mc-btn-action">
-                    <i class="fa fa-bars"></i>
-                </a>
-                
-            </article>
-        </div>
-        <div class="col-md-4 col-sm-6 col-xs-12">
-            <article class="material-card Orange">
-                <h2>
-                    <span>Terrassement</span>
-                    
-                </h2>
-                <div class="mc-content">
-                    <div class="img-container">
-                        <img class="img-responsive" src="img/paves.jpg">
-                    </div>
-                    <div class="mc-description">
-                        Description
-                    </div>
-                </div>
-                <a class="mc-btn-action">
-                    <i class="fa fa-bars"></i>
-                </a>
-                
-                
-            </article>
-        </div>
-        <div class="col-md-4 col-sm-6 col-xs-12">
-            <article class="material-card Blue">
-                <h2>
-                    <span>Terrasse</span>
-                    
-                </h2>
-                <div class="mc-content">
-                    <div class="img-container">
-                        <img class="img-responsive" src="img/paves.jpg">
-                    </div>
-                    <div class="mc-description">
-                        Description
-                    </div>
-                </div>
-                <a class="mc-btn-action">
-                    <i class="fa fa-bars"></i>
-                </a>
-                
-            </article>
-        </div>
-<div class="col-md-4 col-sm-6 col-xs-12">
-            <article class="material-card Orange">
-                <h2>
-                    <span>Terrasse</span>
-                    
-                </h2>
-                <div class="mc-content">
-                    <div class="img-container">
-                        <img class="img-responsive" src="img/paves.jpg">
-                    </div>
-                    <div class="mc-description">
-                        Description
-                    </div>
-                </div>
-                <a class="mc-btn-action">
-                    <i class="fa fa-bars"></i>
-                </a>
-                
-            </article>
-        </div>
+<div style='display:none'>
+  <div id="test2Content" class="inner-overlay-box">
+    <div class="media">
+      <a href="#" class="img">
+        <img alt="terrasse" src="img/terrasse.jpg" class="img-inner-window" /></a>
 
-        <div class="col-md-4 col-sm-6 col-xs-12">
-            <article class="material-card Blue">
-                <h2>
-                    <span>Paves</span>
-                    
-                </h2>
-                <div class="mc-content">
-                    <div class="img-container">
-                        <img class="img-responsive" src="img/paves.jpg">
-                    </div>
-                    <div class="mc-description">
-                        Description
-                    </div>
-                </div>
-                <a class="mc-btn-action">
-                    <i class="fa fa-bars"></i>
-                </a>
-                
-            </article>
-        </div>
+      <div class="hd"><h3>Terrasse</h3></div>
+      <div class="bd">
+        <p class="p-inner-window">Réalisation d'une terrasse de 200 m2 en béton armé, avec une finition à la taloche mécanique qui permettra d'accueillir du carrelage ou du composite, etc... </p> </div>
+    </div>
+  </div>
+</div>
+
+<div style='display:none'>
+  <div id="test3Content" class="inner-overlay-box">
+    <div class="media">
+      <a href="#" class="img">
+        <img alt="dallage" src="img/dallage.jpg" class="img-inner-window" /></a>
+
+      <div class="hd"><h3>Dallage</h3></div>
+      <div class="bd">
+        <p class="p-inner-window">Pose de 40 m2 de dalle en béton reconstitué.</p> </div>
+    </div>
+  </div>
+</div>
+
+<div style='display:none'>
+  <div id="test4Content" class="inner-overlay-box">
+    <div class="media">
+      <a href="#" class="img">
+        <img alt="brique" src="img/brique.jpg" class="img-inner-window" /></a>
+
+      <div class="hd"><h3>Brique</h3></div>
+      <div class="bd">
+        <p class="p-inner-window">Pose de brique de manière traditionelle en respectant le patrinmoine aux alentours.</p> </div>
+    </div>
+  </div>
+</div>
+
+<div style='display:none'>
+  <div id="test5Content" class="inner-overlay-box">
+    <div class="media">
+      <a href="#" class="img">
+        <img alt="cintre en brique" src="img/brique1.jpg" class="img-inner-window" /></a>
+      <div class="hd"><h3>Cintre en brique</h3></div>
+      <div class="bd">
+        <p class="p-inner-window">Réalisation d'un cintre en brique de 33 cm de large et de 3m de long.</p> </div>
+    </div>
+  </div>
+</div>
+
+<div style='display:none'>
+  <div id="test6Content" class="inner-overlay-box">
+    <div class="media">
+      <a href="#" class="img">
+        <img alt="appui en brique" src="img/brique2.jpg" class="img-inner-window" /></a>
+
+      <div class="hd"><h3>Appui en brique</h3></div>
+      <div class="bd">
+        <p class="p-inner-window">Pose d'un appui de fenêtre en brique.</p> </div>
+    </div>
+  </div>
+</div>
+
+<div style='display:none'>
+  <div id="test7Content" class="inner-overlay-box">
+    <div class="media">
+      <a href="#" class="img">
+        <img alt="dalle" src="img/dalle.jpg" class="img-inner-window" /></a>
+
+      <div class="hd"><h3>Dalle</h3></div>
+      <div class="bd">
+        <p class="p-inner-window">Réalisation d'une dalle béton pour la descente de sous-sol.</p> </div>
+    </div>
+  </div>
+</div>
+
+<div style='display:none'>
+  <div id="test8Content" class="inner-overlay-box">
+    <div class="media">
+      <a href="#" class="img">
+        <img alt="facade" src="img/facade.jpg" class="img-inner-window" /></a>
+      <div class="hd"><h3>Façade</h3></div>
+      <div class="bd">
+        <p class="p-inner-window">...</p> </div>
+    </div>
+  </div>
+</div>
+
+<div style='display:none'>
+  <div id="test9Content" class="inner-overlay-box">
+
+    <div class="media">
+      <a href="#" class="img">
+        <img alt="" src="img/dallethimert.jpg" class="img-inner-window" /></a>
+
+      <div class="hd"><h3>Dalle</h3></div>
+      <div class="bd">
+        <p class="p-inner-window">...</p> </div>
+    </div>
+  </div>
+</div>
+
+<div style='display:none'>
+  <div id="test10Content" class="inner-overlay-box">
+
+    <div class="media">
+      <a href="#" class="img">
+        <img alt="terrasse" src="img/terrasse1.jpg" class="img-inner-window" /></a>
+
+      <div class="hd"><h3>Terrasse</h3></div>
+      <div class="bd">
+        <p class="p-inner-window">...</p> </div>
 
     </div>
+  </div>
+</div>
 
+<div style='display:none'>
+  <div id="test11Content" class="inner-overlay-box">
+
+    <div class="media">
+      <a href="#" class="img">
+        <img alt="tranchée" src="img/tranchee.jpg" class="img-inner-window" /></a>
+
+      <div class="hd"><h3>Tranchée</h3></div>
+      <div class="bd">
+        <p class="p-inner-window">...</p> </div>
+    </div>
+  </div>
+</div>
+
+<div style='display:none'>
+  <div id="test12Content" class="inner-overlay-box">
+
+    <div class="media">
+      <a href="#" class="img">
+        <img alt="" src="img/terrasse3.jpg" class="img-inner-window" /></a>
+
+      <div class="hd"><h3>Terrasse</h3></div>
+      <div class="bd">
+        <p class="p-inner-window">...</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!--<div style='display:none'>
+  <div id="test13Content" class="inner-overlay-box">
+
+    <div class="media">
+      <a href="#" class="img">
+        <img alt="" src="http://placehold.it/384x256/" class="img-inner-window" /></a>
+
+      <div class="hd"><h3>Image 13</h3></div>
+      <div class="bd">
+        <p class="p-inner-window">Consequat ea Investigationes in enim congue. Option velit volutpat quod blandit ex. Congue parum praesent aliquam nam clari. Qui praesent quam sollemnes id vulputate. In imperdiet diam at sequitur et. Minim delenit in dolor dolore typi.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div style='display:none'>
+  <div id="test14Content" class="inner-overlay-box">
+
+    <div class="media">
+      <a href="#" class="img">
+        <img alt="" src="http://placehold.it/384x256/" class="img-inner-window" /></a>
+
+      <div class="hd"><h3>Image 14</h3></div>
+      <div class="bd">
+        <p class="p-inner-window">Consequat ea Investigationes in enim congue. Option velit volutpat quod blandit ex. Congue parum praesent aliquam nam clari. Qui praesent quam sollemnes id vulputate. In imperdiet diam at sequitur et. Minim delenit in dolor dolore typi.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div style='display:none'>
+  <div id="test15Content" class="inner-overlay-box">
+
+    <div class="media">
+      <a href="#" class="img">
+        <img alt="" src="http://placehold.it/384x256/" class="img-inner-window" /></a>
+
+      <div class="hd"><h3>Image 15</h3></div>
+      <div class="bd">
+        <p class="p-inner-window">Consequat ea Investigationes in enim congue. Option velit volutpat quod blandit ex. Congue parum praesent aliquam nam clari. Qui praesent quam sollemnes id vulputate. In imperdiet diam at sequitur et. Minim delenit in dolor dolore typi.</p>
+      </div>
+    </div>
+  </div>
+</div>-->
+
+</section>
 
     <h1 class="sec04">Contact</h1>
     
@@ -315,12 +354,17 @@
 </form>
 </div>
 
-<footer>
-  <a class="fa fa-fw fa-facebook"></a>
-</footer>
+
+            <footer class="wrapper">     
+              <h4>2016 &copy; <a href="#" target="_blank">Emilie Chartier</a></h4>
+              
+            </footer>
+       
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 <script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.min.js'></script>
 <script src="js/index.js"></script>
+
 
 </body>
 </html>
